15ff6c45ae12a24ce0c390806fb1c05d8094f877
Vadim Prokoshin <vpro85@gmail.com>
2024-04-30 23:52:48 +0300

    6.5. Registering the component in the store.module.ts file in the src/app/store folder

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { StoreComponent } from './store.component';
 import { CounterDirective } from './counter.directive';
+import { CartSummaryComponent } from './cartSummary.component';
 
 @NgModule({
   imports: [ModelModule, BrowserModule, FormsModule],
-  declarations: [StoreComponent, CounterDirective],
+  declarations: [StoreComponent, CounterDirective, CartSummaryComponent],
   exports: [StoreComponent],
 })
 export class StoreModule {}
