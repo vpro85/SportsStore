85f5373021c6b4d7b5822b3566cd19ea049d8424
Vadim Prokoshin <vpro85@gmail.com>
2024-05-01 00:32:02 +0300

    6.11. Creating the routing configuration in the app.module.ts file in the src/app folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from './store/store.module';
+import { RouterModule } from '@angular/router';
+import { StoreComponent } from './store/store.component';
+import { CartDetailComponent } from './store/cartDetail.component';
+import { CheckoutComponent } from './store/checkout.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, StoreModule],
+  imports: [
+    BrowserModule,
+    StoreModule,
+    RouterModule.forRoot([
+      { path: 'store', component: StoreComponent },
+      { path: 'cart', component: CartDetailComponent },
+      { path: 'checkout', component: CheckoutComponent },
+      { path: '**', redirectTo: '/store' },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
