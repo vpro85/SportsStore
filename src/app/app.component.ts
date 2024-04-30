626e1eb325b99c9c0ff52734272fc197a8e4f57a
Vadim Prokoshin <vpro85@gmail.com>
2024-04-30 11:48:16 +0300

    5.18. Adding an element in the app.component.ts file in the src/app folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: `<div class="bg-success p-2 text-center text-white">
-    This is SportsStore
-  </div>`,
+  template: "<store></store>",
 })
 export class AppComponent {}
