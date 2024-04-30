c5a971559f8131abbc9023c1e51982fa6a44f5cb
Vadim Prokoshin <vpro85@gmail.com>
2024-04-30 23:56:59 +0300

    6.6. Adding cart support in the store.component.ts file in the src/app/store folder

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,7 @@
 import { Component, Signal, computed, signal } from '@angular/core';
 import { Product } from '../model/product.model';
 import { ProductRepository } from '../model/product.repository';
+import { Cart } from '../model/cart.model';
 
 @Component({
   selector: 'store',
@@ -13,10 +14,9 @@ export class StoreComponent {
   productsPerPage = signal(4);
   selectedPage = signal(1);
   pagedProducts: Signal<Product[]>;
-  // pageNumbers: Signal<number[]>;
   pageCount: Signal<number>;
 
-  constructor(private repository: ProductRepository) {
+  constructor(private repository: ProductRepository, private cart: Cart) {
     this.products = computed(() => {
       if (this.selectedCategory() == undefined) {
         return this.repository.products();
@@ -26,21 +26,20 @@ export class StoreComponent {
           .filter((p) => p.category === this.selectedCategory());
       }
     });
+
     this.categories = repository.categories;
+
     let pageIndex = computed(() => {
       return (this.selectedPage() - 1) * this.productsPerPage();
     });
+
     this.pagedProducts = computed(() => {
       return this.products().slice(
         pageIndex(),
         pageIndex() + this.productsPerPage()
       );
     });
-    // this.pageNumbers = computed(() => {
-    //   return Array(Math.ceil(this.products().length / this.productsPerPage()))
-    //     .fill(0)
-    //     .map((x, i) => i + 1);
-    // });
+
     this.pageCount = computed(() => {
       return Math.ceil(this.products().length / this.productsPerPage());
     });
@@ -58,5 +57,9 @@ export class StoreComponent {
   changePageSize(newSize: number) {
     this.productsPerPage.set(Number(newSize));
     this.changePage(1);
+  }
+
+  addProductToCart(product: Product) {
+    this.cart.addLine(product);
   }
 }
