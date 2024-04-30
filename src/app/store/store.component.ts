796328a735ddde56c1e2fc768f35bcf34d8e80dc
Vadim Prokoshin <vpro85@gmail.com>
2024-04-30 14:05:44 +0300

    5.23. Adding pagination in the store.component.ts file in the src/app/store folder

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -10,6 +10,10 @@ export class StoreComponent {
   products: Signal<Product[]>;
   categories: Signal<string[]>;
   selectedCategory = signal<string | undefined>(undefined);
+  productsPerPage = signal(4);
+  selectedPage = signal(1);
+  pagedProducts: Signal<Product[]>;
+  pageNumbers: Signal<number[]>;
 
   constructor(private repository: ProductRepository) {
     this.products = computed(() => {
@@ -22,9 +26,33 @@ export class StoreComponent {
       }
     });
     this.categories = repository.categories;
+    let pageIndex = computed(() => {
+      return (this.selectedPage() - 1) * this.productsPerPage();
+    });
+    this.pagedProducts = computed(() => {
+      return this.products().slice(
+        pageIndex(),
+        pageIndex() + this.productsPerPage()
+      );
+    });
+    this.pageNumbers = computed(() => {
+      return Array(Math.ceil(this.products().length / this.productsPerPage()))
+        .fill(0)
+        .map((x, i) => i + 1);
+    });
   }
 
   changeCategory(newCategory?: string) {
     this.selectedCategory.set(newCategory);
+    this.changePage(1);
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage.set(newPage);
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage.set(Number(newSize));
+    this.changePage(1);
   }
 }
