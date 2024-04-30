a9d928c2faf7f21683118356648add90c30c4a0a
Vadim Prokoshin <vpro85@gmail.com>
2024-04-30 03:22:27 +0300

    5.8. Replacing the contents of the app.component.ts file in the src/app folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app',
+  template: `<div class="bg-success p-2 text-center text-white">
+    This is SportsStore
+  </div>`,
 })
-export class AppComponent {
-  title = 'SportsStore';
-}
+export class AppComponent {}
