f30d357f4a36dd553a7d8312f4ce9264f2560f44
Vadim Prokoshin <vpro85@gmail.com>
2024-04-30 13:01:55 +0300

    5.21. Category filtering in the store.component.ts file in the src/app/store folder

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,17 +1,30 @@
-import { Component, Signal } from '@angular/core';
+import { Component, Signal, computed, signal } from '@angular/core';
 import { Product } from '../model/product.model';
 import { ProductRepository } from '../model/product.repository';
 
 @Component({
-  selector: "store",
-  templateUrl: "store.component.html",
+  selector: 'store',
+  templateUrl: 'store.component.html',
 })
 export class StoreComponent {
   products: Signal<Product[]>;
   categories: Signal<string[]>;
+  selectedCategory = signal<string | undefined>(undefined);
 
   constructor(private repository: ProductRepository) {
-    this.products = repository.products;
+    this.products = computed(() => {
+      if (this.selectedCategory() == undefined) {
+        return this.repository.products();
+      } else {
+        return this.repository
+          .products()
+          .filter((p) => p.category === this.selectedCategory());
+      }
+    });
     this.categories = repository.categories;
+  }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory.set(newCategory);
   }
 }
