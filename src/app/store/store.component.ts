4f1dc923f41d57103e3540cea573a6e94794da90
Vadim Prokoshin <vpro85@gmail.com>
2024-04-30 11:38:11 +0300

    5.15. The contents of the store.component.ts file in the src/app/store folder

---
+++ src/app/store/store.component.ts
@@ -0,0 +1,17 @@
+import { Component, Signal } from '@angular/core';
+import { Product } from '../model/product.model';
+import { ProductRepository } from '../model/product.repository';
+
+@Component({
+  selector: 'store',
+  templateUrl: 'store.component.html',
+})
+export class StoreComponent {
+  products: Signal<Product[]>;
+  categories: Signal<string[]>;
+
+  constructor(private repository: ProductRepository) {
+    this.products = repository.products;
+    this.categories = repository.categories;
+  }
+}
