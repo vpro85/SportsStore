c0b45a71eb018bd6acbe1ce4d69f6e31b3752ee9
Vadim Prokoshin <vpro85@gmail.com>
2024-05-11 00:04:11 +0300

    7.8. Adding authentication in the rest.datasource.ts file in the src/app/model folder

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Product } from './product.model';
 import { Order } from './order.model';
 
@@ -10,6 +10,7 @@ const PORT = 3500;
 @Injectable()
 export class RestDataSource {
   baseUrl: string;
+  auth_token?: string;
 
   constructor(private http: HttpClient) {
     this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
@@ -21,5 +22,19 @@ export class RestDataSource {
 
   saveOrder(order: Order): Observable<Order> {
     return this.http.post<Order>(this.baseUrl + 'orders', order);
+  }
+
+  authenticate(user: string, pass: string): Observable<boolean> {
+    return this.http
+      .post<any>(this.baseUrl + 'login', {
+        name: user,
+        password: pass,
+      })
+      .pipe(
+        map((response) => {
+          this.auth_token = response.success ? response.token : null;
+          return response.success;
+        })
+      );
   }
 }
