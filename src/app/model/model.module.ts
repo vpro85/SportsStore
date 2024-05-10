75b3da5a469699fde21c1314fd745c1706693caa
Vadim Prokoshin <vpro85@gmail.com>
2024-05-10 19:54:56 +0300

    6.28. Adding a service in the model.module.ts file in the src/app/model folder

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -4,14 +4,18 @@ import { StaticDataSource } from './static.datasource';
 import { Cart } from './cart.model';
 import { Order } from './order.model';
 import { OrderRepository } from './order.repository';
+import { HttpClientModule } from '@angular/common/http';
+import { RestDataSource } from './rest.datasource';
 
 @NgModule({
+  imports: [HttpClientModule],
   providers: [
     ProductRepository,
     StaticDataSource,
     Cart,
     Order,
     OrderRepository,
+    RestDataSource,
   ],
 })
 export class ModelModule {}
