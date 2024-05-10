71d53ff5f2dd7880e283de47b49434df3c2cbb88
Vadim Prokoshin <vpro85@gmail.com>
2024-05-10 22:48:56 +0300

    6.30. Using a new data source in the order.repository.ts file in the src/app/model folder

--- src/app/model/order.repository.ts
+++ src/app/model/order.repository.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { StaticDataSource } from './static.datasource';
 import { Order } from './order.model';
 import { Observable } from 'rxjs';
+import { RestDataSource } from './rest.datasource';
 
 @Injectable()
 export class OrderRepository {
-  constructor(private dataSource: StaticDataSource) {}
+  constructor(private dataSource: RestDataSource) {}
 
   saveOrder(order: Order): Observable<Order> {
     return this.dataSource.saveOrder(order);
