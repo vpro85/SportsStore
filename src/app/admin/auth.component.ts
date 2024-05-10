c62815c69c1e1020ede04fe3f56c8f1ee1ad6858
Vadim Prokoshin <vpro85@gmail.com>
2024-05-10 23:02:42 +0300

    7.1. The content of the auth.component.ts file in the src/app/admin folder

---
+++ src/app/admin/auth.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  templateUrl: 'auth.component.html',
+})
+export class AuthComponent {
+  username?: string;
+  password?: string;
+  errorMessage?: string;
+
+  constructor(private router: Router) {}
+
+  authenticate(form: NgForm) {
+    if (form.valid) {
+      //perform authentication
+      this.router.navigateByUrl('/admin/main');
+    } else {
+      this.errorMessage = 'Form Data Invalid';
+    }
+  }
+}
