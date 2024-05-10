50bc89e7c55eda3fc499bc4e645ce05df63a787b
Vadim Prokoshin <vpro85@gmail.com>
2024-05-11 01:48:09 +0300

    7.24. Adding features in the material.module.ts file in the src/app/admin folder

--- src/app/admin/material.module.ts
+++ src/app/admin/material.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
-const features: any[] = [];
+const features: any[] = [
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatDividerModule,
+  MatButtonModule,
+];
 @NgModule({
-  imports: [],
-  exports: [],
+  imports: [features],
+  exports: [features],
 })
 export class MaterialFeatures {}
