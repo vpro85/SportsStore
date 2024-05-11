e955e18ce8e664d047c11c7cb6e074f49637182e
Vadim Prokoshin <vpro85@gmail.com>
2024-05-11 13:28:24 +0300

    7.29. The productTable.component.html file in the src/app/admin folder

--- src/app/admin/productTable.component.ts
+++ src/app/admin/productTable.component.ts
@@ -1,7 +1,6 @@
 import { Component } from "@angular/core";
 
 @Component({
-    template: `
-    <h3 style="padding-top: 10px">Product table Placeholder</h3>`
+    templateUrl: "productTable.component.html"
 })
 export class ProductTableComponent{}
