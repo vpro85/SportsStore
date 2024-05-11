1b61b97d9b74dfa70f87c46637786a0ed58c037f
Vadim Prokoshin <vpro85@gmail.com>
2024-05-11 14:07:11 +0300

    7.31. Adding features in the productTable.component.ts file in the src/app/admin folder

--- src/app/admin/productTable.component.ts
+++ src/app/admin/productTable.component.ts
@@ -1,6 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, IterableDiffer, IterableDiffers } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Product } from '../model/product.model';
+import { ProductRepository } from '../model/product.repository';
 
 @Component({
-    templateUrl: "productTable.component.html"
+  templateUrl: 'productTable.component.html',
 })
-export class ProductTableComponent{}
+export class ProductTableComponent {
+  colsAndRows: string[] = ['id', 'name', 'category', 'price', 'buttons'];
+  dataSource = new MatTableDataSource<Product>(this.repository.products());
+  differ: IterableDiffer<Product>;
+
+  constructor(private repository: ProductRepository, differs: IterableDiffers) {
+    this.differ = differs.find(this.repository.products()).create();
+  }
+
+  ngDoCheck() {
+    let changes = this.differ?.diff(this.repository.products());
+    if (changes != null) {
+      this.dataSource.data = this.repository.products();
+    }
+  }
+
+  deleteProduct(id: number) {
+    this.repository.deleteProduct(id);
+  }
+}
